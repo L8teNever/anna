ae4ef6ec8f188a32100ba5715dbe07afb940bb9b
L8tenever <info@l8tnever.com>
2026-07-17 18:37:03 -0700

    feat: implement PWA service worker with offline-caching support and versioning

--- public/js/version.js
+++ public/js/version.js
@@ -10,5 +10,5 @@
  * Stellen bei jeder Änderung gemeinsam hochzählen!
  */
 (function (root) {
-  root.APP_VERSION = "3.18.0";
+  root.APP_VERSION = "3.19.0";
 })(typeof self !== "undefined" ? self : this);
